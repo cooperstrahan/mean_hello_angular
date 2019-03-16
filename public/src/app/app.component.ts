8fe658d226448a1952acac57bd0b314dca15a0ec
cstra <cooperstrahan@gmail.com>
2019-03-15 18:27:11 -0700

    Restful Tasks

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'public';
+  constructor(private _httpService: HttpService){
+    _httpService.getTasks().subscribe(data => console.log("Got our tasks!", data))
+    _httpService.getTask('5c8ac89e8d40f8624dbc343c').subscribe(data => console.log("Got our task!", data))
+    // _httpService.deleteTask(id).subscribe(data => console.log("Deleted the task!", data))
+  }
 }
---
+++ public/src/app/http.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private _http: HttpClient) { 
+    // this.getTasks();
+    // this.getTask('5c8ac89e8d40f8624dbc343c');
+    // this.deleteTask('');
+  }
+
+  getTasks(){
+    return this._http.get('/tasks');
+  }
+  getTask(id: String){
+    return this._http.get('/tasks/'+id);
+  }
+  deleteTask(id: string){
+    return this._http.delete('/tasks'+id);
+  }
+}
+
